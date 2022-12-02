df50d9c17909ac1130c1805add1378cb6519f8e0
Rémi Pradal <remi.p@foodles.co>
2022-12-02 22:34:06 +0100

    Day 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,16 @@
 import day1.solveDay1Part1Puzzle
 import day1.solveDay1Part2Puzzle
+import day2.solveDay2Part1Puzzle
+import day2.solveDay2Part2Puzzle
 import java.io.File
 
 fun main() {
     println("Day1")
     println("Part1: ${solveDay1Part1Puzzle(File("src/main/kotlin/day1/input"))}")
     println("Part2: ${solveDay1Part2Puzzle(File("src/main/kotlin/day1/input"))}")
+
+    println("Day2")
+    println("Part1: ${solveDay2Part1Puzzle(File("src/main/kotlin/day2/input"))}")
+    println("Part2: ${solveDay2Part2Puzzle(File("src/main/kotlin/day2/input"))}")
 }
 
---
+++ src/test/kotlin/day2/Day2KtTest.kt
@@ -0,0 +1,27 @@
+package day2
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import java.io.File
+
+internal class Day2KtTest {
+
+    @Test
+    fun solveDay2Part1Puzzle() {
+        val testFile = File("src/test/kotlin/day2/sample")
+
+        val result = solveDay2Part1Puzzle(testFile)
+
+        assertEquals(15, result)
+    }
+
+    @Test
+    fun solveDay1Part2Puzzle() {
+        val testFile = File("src/test/kotlin/day2/sample")
+
+        val result = solveDay2Part2Puzzle(testFile)
+
+        assertEquals(12, result)
+    }
+}
