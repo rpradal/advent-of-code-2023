9a428f16abb02619e0c2879f58588597a761c052
Rémi Pradal <remi.p@foodles.co>
2022-12-03 10:22:21 +0100

    Day 3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,6 +2,8 @@ import day1.solveDay1Part1Puzzle
 import day1.solveDay1Part2Puzzle
 import day2.solveDay2Part1Puzzle
 import day2.solveDay2Part2Puzzle
+import day3.solveDay3Part1Puzzle
+import day3.solveDay3Part2Puzzle
 import java.io.File
 
 fun main() {
@@ -12,5 +14,9 @@ fun main() {
     println("Day2")
     println("Part1: ${solveDay2Part1Puzzle(File("src/main/kotlin/day2/input"))}")
     println("Part2: ${solveDay2Part2Puzzle(File("src/main/kotlin/day2/input"))}")
+
+    println("Day3")
+    println("Part1: ${solveDay3Part1Puzzle(File("src/main/kotlin/day3/input"))}")
+    println("Part2: ${solveDay3Part2Puzzle(File("src/main/kotlin/day3/input"))}")
 }
 
