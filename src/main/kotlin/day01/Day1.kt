6d9f2740d062601618f84e3ec0217c8e2d14d1fd
Rémi Pradal <remi.p@foodles.co>
2023-12-01 19:40:17 +0100

    Day1Part1

--- src/main/kotlin/day01/Day1.kt
+++ src/main/kotlin/day01/Day1.kt
@@ -3,18 +3,9 @@ package day01
 import java.io.File
 
 fun solveDay1Part1Puzzle(file: File): Int {
-    return extractElvesCarrying(file).maxOf { it }
-}
-
-fun solveDay1Part2Puzzle(file: File): Int {
-    return extractElvesCarrying(file).sortedDescending().take(3).sum()
-}
-
-private fun extractElvesCarrying(file: File): List<Int> {
     return file
         .readLines()
-        .joinToString(",")
-        .split(",,")
-        .map { it.split(",").map(String::toInt) }
-        .map { it.sum() }
+        .map { "${it.first(Char::isDigit)}${it.last(Char::isDigit)}" }
+        .map(String::toInt)
+        .sum()
 }
--- src/test/kotlin/day01/Day1KtTest.kt
+++ src/test/kotlin/day01/Day1KtTest.kt
@@ -2,25 +2,32 @@ package day01
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import java.io.File
 
 internal class Day1KtTest {
+    companion object {
+        @JvmStatic
+        fun generatePart1TestCases(): List<Arguments> {
+            return listOf(
+                Arguments.of("src/test/kotlin/day01/sample_part1", 142),
+                Arguments.of("src/test/kotlin/day01/puzzle", 56506),
+            )
+        }
+    }
 
-    @Test
-    fun solveDay1Part1Puzzle() {
-        val testFile = File("src/test/kotlin/day01/sample")
+    @ParameterizedTest
+    @MethodSource("generatePart1TestCases")
+    fun solveDay1Part1Puzzle(
+        path: String,
+        expectedResult: Int,
+    ) {
+        val testFile = File(path)
 
         val result = solveDay1Part1Puzzle(testFile)
 
-        assertEquals(24000, result)
-    }
-
-    @Test
-    fun solveDay1Part2Puzzle() {
-        val testFile = File("src/test/kotlin/day01/sample")
-
-        val result = solveDay1Part2Puzzle(testFile)
-
-        assertEquals(45000, result)
+        assertEquals(expectedResult, result)
     }
 }
